edfc96b78fb690c33f3b1f0977f63eea39afe86b
Heorhii Pluhovyi <heorhiipluhovui@gmail.com>
2023-02-10 12:47:04 +0200

    add in readMe Demo link

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [ type, setType ] = useState('users');
+  const [ data, seTdata ] = useState([]);
+  const [ position, setPosition ] = useState({
+    x: 0, 
+    y: 0
+  })
+
+  // useEffect(() => {
+  //   console.log('render');
+  // })
+  console.log('render of app component')
+
+  useEffect(() => {
+    console.log(`UseEffect: Type change to ${type}`);
+    fetch(`https://jsonplaceholder.typicode.com/${type}`)
+      .then(response => response.json())
+      .then(json => {
+        console.log(json);
+
+        seTdata(json);
+      });
+  }, [type])
+
+  const mouseMoveHandler: { (event: MouseEvent): void } = (event: MouseEvent) => {
+    setPosition({
+      x: event.clientX,
+      y: event.clientY
+    })
+  }
+
+  useEffect(() => {
+    console.log('componentDidMount');
+
+    window.addEventListener('mousemove', mouseMoveHandler);
+
+    return () => {
+      window.removeEventListener('mousemove', mouseMoveHandler)
+    }
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Resource: { type }</h1>
+
+      <div className="App__container--buttons-blok ">
+        <button onClick={ () => setType('users')} className="App--button button-primary">Users</button>
+        <button onClick={ () => setType('todos')} className="App--button button-reset">Todos</button>
+        <button onClick={ () => setType('posts')} className="App--button button-danger">Posts</button>
+      </div>
+
+      <pre>{ JSON.stringify(position, null, 2) }</pre>
+      <pre className="JSON-list">{ JSON.stringify(data, null, 2) }</pre>
     </div>
   );
 }
